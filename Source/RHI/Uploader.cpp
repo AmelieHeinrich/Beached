88bc11963b31ee8a7b4a79e22c311b900b9c29e4
Amélie Heinrich <MikuoH15TH@gmail.com>
2024-12-30 23:20:57 +0100

    [FIX] Coding style changes

--- Source/RHI/Uploader.cpp
+++ Source/RHI/Uploader.cpp
@@ -30,21 +30,21 @@ void Uploader::EnqueueTextureUpload(Vector<UInt8> buffer, Ref<Resource> texture)
     
     D3D12_RESOURCE_DESC desc = texture->GetResource()->GetDesc();
     std::vector<D3D12_PLACED_SUBRESOURCE_FOOTPRINT> footprints(desc.MipLevels);
-    std::vector<UInt32> num_rows(desc.MipLevels);
-    std::vector<UInt64> row_sizes(desc.MipLevels);
+    std::vector<UInt32> numRows(desc.MipLevels);
+    std::vector<UInt64> rowSizes(desc.MipLevels);
     uint64_t totalSize = 0;
 
-    sData.Device->GetDevice()->GetCopyableFootprints(&desc, 0, desc.MipLevels, 0, footprints.data(), num_rows.data(), row_sizes.data(), &totalSize);
+    sData.Device->GetDevice()->GetCopyableFootprints(&desc, 0, desc.MipLevels, 0, footprints.data(), numRows.data(), rowSizes.data(), &totalSize);
     request.StagingBuffer = MakeRef<Buffer>(sData.Device, sData.Heaps, totalSize, 0, BufferType::Copy, "Staging Buffer " + texture->GetName());
 
     UInt8 *pixels = reinterpret_cast<UInt8*>(buffer.data());    
     UInt8* mapped;
     request.StagingBuffer->Map(0, 0, (void**)&mapped);
     for (int i = 0; i < desc.MipLevels; i++) {
-        for (int j = 0; j < num_rows[i]; j++) {
-            memcpy(mapped, pixels, row_sizes[i]);
+        for (int j = 0; j < numRows[i]; j++) {
+            memcpy(mapped, pixels, rowSizes[i]);
             mapped += footprints[i].Footprint.RowPitch;
-            pixels += row_sizes[i];
+            pixels += rowSizes[i];
         }
     }
     request.StagingBuffer->Unmap(0, 0);
@@ -62,21 +62,21 @@ void Uploader::EnqueueTextureUpload(Image image, Ref<Resource> buffer)
     
     D3D12_RESOURCE_DESC desc = buffer->GetResource()->GetDesc();
     std::vector<D3D12_PLACED_SUBRESOURCE_FOOTPRINT> footprints(desc.MipLevels);
-    std::vector<UInt32> num_rows(desc.MipLevels);
-    std::vector<UInt64> row_sizes(desc.MipLevels);
+    std::vector<UInt32> numRows(desc.MipLevels);
+    std::vector<UInt64> rowSizes(desc.MipLevels);
     uint64_t totalSize = 0;
 
-    sData.Device->GetDevice()->GetCopyableFootprints(&desc, 0, desc.MipLevels, 0, footprints.data(), num_rows.data(), row_sizes.data(), &totalSize);
+    sData.Device->GetDevice()->GetCopyableFootprints(&desc, 0, desc.MipLevels, 0, footprints.data(), numRows.data(), rowSizes.data(), &totalSize);
     request.StagingBuffer = MakeRef<Buffer>(sData.Device, sData.Heaps, totalSize, 0, BufferType::Copy, "Staging Buffer " + buffer->GetName());
 
     UInt8 *pixels = reinterpret_cast<UInt8*>(image.Pixels.data());    
     UInt8* mapped;
     request.StagingBuffer->Map(0, 0, (void**)&mapped);
     for (int i = 0; i < desc.MipLevels; i++) {
-        for (int j = 0; j < num_rows[i]; j++) {
-            memcpy(mapped, pixels, row_sizes[i]);
+        for (int j = 0; j < numRows[i]; j++) {
+            memcpy(mapped, pixels, rowSizes[i]);
             mapped += footprints[i].Footprint.RowPitch;
-            pixels += row_sizes[i];
+            pixels += rowSizes[i];
         }
     }
     request.StagingBuffer->Unmap(0, 0);
